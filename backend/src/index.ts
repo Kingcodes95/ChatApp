ef13324737153fbbf5fc6c16a623510d1975393b
Ronnie Perez <Ronaldaperez95@gmail.com>
2024-11-15 19:04:46 -0600

    Added logic for signup and fixed a bug in signup that was overloading the auth.route.ts file needed to make return res.; => res. return;

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
 import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
